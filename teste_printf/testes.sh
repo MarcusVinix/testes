ca31820b2c39b08b5a101e056e59729a355d952a
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-30 18:47:25 -0300

    update

--- teste_printf/testes.sh
+++ teste_printf/testes.sh
@@ -21,7 +21,6 @@ cd ~
 echo Test PFT_2019
 cd ft_printf
 git clone https://github.com/gavinfielder/pft.git pft_2019 && echo "pft_2019/" >> .gitignore && cd pft_2019 && rm unit_tests.c && rm options-config.ini && git clone https://github.com/cclaude42/PFT_2019.git temp && cp temp/unit_tests.c . && cp temp/options-config.ini . && rm -rf temp
-cd pft_2019
 make re
 ./test
 cd ~
@@ -31,7 +30,7 @@ echo Test 42TESTERS-PRINTF
 cd ft_printf
 git clone https://github.com/Mazoise/42TESTERS-PRINTF.git
 cd 42TESTERS-PRINTF
-bash run_test.sh
+bash runtest.sh
 cd ~
 
 echo Cleaning
